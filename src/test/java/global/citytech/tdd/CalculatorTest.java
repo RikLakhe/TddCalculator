ead3fbc7442bc25fc345a7e94132f4d7d5dca5ab
Rikesh Lal Shrestha <rikesh.shrestha@citytech.global>
2020-08-27 23:04:56 +0545

    scientific test added

--- src/test/java/global/citytech/tdd/CalculatorTest.java
+++ src/test/java/global/citytech/tdd/CalculatorTest.java
@@ -29,10 +29,60 @@ public class CalculatorTest {
         }
 
         public void divideNumbers(double firstNumber, double secondNumber) {
-            if(secondNumber == 0){
+            if (secondNumber == 0) {
                 throw new IllegalArgumentException("Cannot be divided by Zero! Its infinity.");
-            }else{
+            } else {
                 result = firstNumber / secondNumber;
+            }
+        }
+
+        public void powerNumber(double firstNumber, double secondNumber) {
+            result = Math.pow(firstNumber, secondNumber);
+        }
+
+        public void squareRoot(double firstNumber) {
+            if (firstNumber < 0) {
+                throw new IllegalArgumentException("Square root of negative number is irrational number.");
+            } else {
+                result = Math.sqrt(firstNumber);
+            }
+        }
+
+        public void cubeRoot(double firstNumber) {
+            result = Math.cbrt(firstNumber);
+        }
+
+        public void sinNumber(double firstNumber) {
+            result = Math.sin(firstNumber);
+        }
+
+        public void cosNumber(double firstNumber) {
+            result = Math.cos(firstNumber);
+        }
+
+        public void tanNumber(double firstNumber) {
+            result = Math.tan(firstNumber);
+        }
+
+        //        Logarithm of a number base e
+        public void lnNumber(double firstNumber) {
+            if (firstNumber == 0) {
+                throw new IllegalArgumentException("Cannot be zero! Its Infinity!");
+            } else if (firstNumber < 0) {
+                throw new IllegalArgumentException("Cannot be negative!");
+            } else {
+                result = Math.log(firstNumber);
+            }
+        }
+
+        //        Logarithm of a number base 10
+        public void logNumber(double firstNumber) {
+            if (firstNumber == 0) {
+                throw new IllegalArgumentException("Cannot be zero! Its Infinity!");
+            } else if (firstNumber < 0) {
+                throw new IllegalArgumentException("Cannot be negative!");
+            } else {
+                result = Math.log10(firstNumber);
             }
         }
     }
@@ -192,9 +242,9 @@ public class CalculatorTest {
     @Test(expected = IllegalArgumentException.class)
     public void divide_SecondNumber_Zero_Testing() {
         Calculator c = new Calculator();
-        try{
+        try {
             c.divideNumbers(10, 0);
-        }catch(IllegalArgumentException err){
+        } catch (IllegalArgumentException err) {
             String message = "Cannot be divided by Zero! Its infinity.";
             assertEquals(message, err.getMessage());
             throw err;
@@ -204,10 +254,225 @@ public class CalculatorTest {
     @Test(expected = IllegalArgumentException.class)
     public void divide_Both_Zero_Testing() {
         Calculator c = new Calculator();
-        try{
+        try {
             c.divideNumbers(0, 0);
-        }catch(IllegalArgumentException err){
+        } catch (IllegalArgumentException err) {
             String message = "Cannot be divided by Zero! Its infinity.";
+            assertEquals(message, err.getMessage());
+            throw err;
+        }
+    }
+
+    @Test
+    public void power_SecondNumber_Testing() {
+        Calculator c = new Calculator();
+        c.powerNumber(2, 6);
+        double receivedResult = c.getResult();
+        assertEquals(64, receivedResult, DELTA);
+    }
+
+    @Test
+    public void power_By_Zero_Testing() {
+        Calculator c = new Calculator();
+        c.powerNumber(2, 0);
+        double receivedResult = c.getResult();
+        assertEquals(1, receivedResult, DELTA);
+    }
+
+    @Test
+    public void power_Of_Negative_Testing() {
+        Calculator c = new Calculator();
+        c.powerNumber(-2, 10);
+        double receivedResult = c.getResult();
+        assertEquals(1024, receivedResult, DELTA);
+        c.powerNumber(-2, 7);
+        receivedResult = c.getResult();
+        assertEquals(-128, receivedResult, DELTA);
+    }
+
+    @Test
+    public void power_By_Negative_Testing() {
+        Calculator c = new Calculator();
+        c.powerNumber(2, -10);
+        double receivedResult = c.getResult();
+        assertEquals(9.765625E-4, receivedResult, DELTA);
+    }
+
+    @Test
+    public void squareRoot_Number_Testing() {
+        Calculator c = new Calculator();
+        c.squareRoot(4);
+        double receivedResult = c.getResult();
+        assertEquals(2, receivedResult, DELTA);
+    }
+
+    @Test
+    public void squareRoot_Zero_Testing() {
+        Calculator c = new Calculator();
+        c.squareRoot(0);
+        double receivedResult = c.getResult();
+        assertEquals(0, receivedResult, DELTA);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void squareRoot_NegativeNumber_Testing() {
+        Calculator c = new Calculator();
+        try {
+            c.squareRoot(-4);
+        } catch (IllegalArgumentException err) {
+            String message = "Square root of negative number is irrational number.";
+            assertEquals(message, err.getMessage());
+            throw err;
+        }
+    }
+
+    @Test
+    public void cubeRoot_Number_Testing() {
+        Calculator c = new Calculator();
+        c.cubeRoot(27);
+        double receivedResult = c.getResult();
+        assertEquals(3, receivedResult, DELTA);
+
+        c.cubeRoot(0);
+        receivedResult = c.getResult();
+        assertEquals(0, receivedResult, DELTA);
+
+        c.cubeRoot(-27);
+        receivedResult = c.getResult();
+        assertEquals(-3, receivedResult, DELTA);
+    }
+
+    @Test
+    public void sin_Number_Testing() {
+        Calculator c = new Calculator();
+        c.sinNumber(60);
+        double receivedResult = c.getResult();
+        assertEquals(-0.3048106211022167, receivedResult, DELTA);
+
+        c.sinNumber(-60);
+        receivedResult = c.getResult();
+        assertEquals(0.3048106211022167, receivedResult, DELTA);
+
+        c.sinNumber(90);
+        receivedResult = c.getResult();
+        assertEquals(0.8939966636005579, receivedResult, DELTA);
+
+        c.sinNumber(-90);
+        receivedResult = c.getResult();
+        assertEquals(-0.8939966636005579, receivedResult, DELTA);
+
+        c.sinNumber(0);
+        receivedResult = c.getResult();
+        assertEquals(0, receivedResult, DELTA);
+    }
+
+    @Test
+    public void cos_Number_Testing() {
+        Calculator c = new Calculator();
+        c.cosNumber(60);
+        double receivedResult = c.getResult();
+        assertEquals(-0.9524129804151563, receivedResult, DELTA);
+
+        c.cosNumber(-60);
+        receivedResult = c.getResult();
+        assertEquals(-0.9524129804151563, receivedResult, DELTA);
+
+        c.cosNumber(90);
+        receivedResult = c.getResult();
+        assertEquals(-0.4480736161291701, receivedResult, DELTA);
+
+        c.cosNumber(-90);
+        receivedResult = c.getResult();
+        assertEquals(-0.4480736161291701, receivedResult, DELTA);
+
+        c.cosNumber(0);
+        receivedResult = c.getResult();
+        assertEquals(1, receivedResult, DELTA);
+    }
+
+    @Test
+    public void tan_Number_Testing() {
+        Calculator c = new Calculator();
+        c.tanNumber(60);
+        double receivedResult = c.getResult();
+        assertEquals(0.320040389379563, receivedResult, DELTA);
+
+        c.tanNumber(-60);
+        receivedResult = c.getResult();
+        assertEquals(-0.320040389379563, receivedResult, DELTA);
+
+        c.tanNumber(90);
+        receivedResult = c.getResult();
+        assertEquals(-1.995200412208242, receivedResult, DELTA);
+
+        c.tanNumber(-90);
+        receivedResult = c.getResult();
+        assertEquals(1.995200412208242, receivedResult, DELTA);
+
+        c.tanNumber(0);
+        receivedResult = c.getResult();
+        assertEquals(0, receivedResult, DELTA);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void ln_Number_Testing() {
+        Calculator c = new Calculator();
+        c.lnNumber(464);
+        double receivedResult = c.getResult();
+        assertEquals(6.139884552226255, receivedResult, DELTA);
+
+        c.lnNumber(1);
+        receivedResult = c.getResult();
+        assertEquals(0, receivedResult, DELTA);
+
+        c.lnNumber(90);
+        receivedResult = c.getResult();
+        assertEquals(4.499809670330265, receivedResult, DELTA);
+
+        try {
+            c.lnNumber(0);
+        } catch (IllegalArgumentException err) {
+            String message = "Cannot be zero! Its Infinity!";
+            assertEquals(message, err.getMessage());
+            throw err;
+        }
+
+        try {
+            c.lnNumber(-1);
+        } catch (IllegalArgumentException err) {
+            String message = "Cannot be negative!";
+            assertEquals(message, err.getMessage());
+            throw err;
+        }
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void log_Number_Testing() {
+        Calculator c = new Calculator();
+        c.logNumber(464);
+        double receivedResult = c.getResult();
+        assertEquals(2.6665179805548807, receivedResult, DELTA);
+
+        c.logNumber(1);
+        receivedResult = c.getResult();
+        assertEquals(0, receivedResult, DELTA);
+
+        c.logNumber(90);
+        receivedResult = c.getResult();
+        assertEquals(1.954242509439325, receivedResult, DELTA);
+
+        try {
+            c.logNumber(0);
+        } catch (IllegalArgumentException err) {
+            String message = "Cannot be zero! Its Infinity!";
+            assertEquals(message, err.getMessage());
+            throw err;
+        }
+
+        try {
+            c.logNumber(-1);
+        } catch (IllegalArgumentException err) {
+            String message = "Cannot be negative!";
             assertEquals(message, err.getMessage());
             throw err;
         }
